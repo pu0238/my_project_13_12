1521f6d1ddd981dcd9b114f3359dbf9915fc4f6b
pu0238 <pu0238@protonmail.com>
2024-12-13 11:07:35 +0000

    Updated frontend

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
 
 const rates = ref([])
+const iloscWaluty = ref(0)
 
 const getDataFromNBP = async () => {
   const res = await fetch("https://api.nbp.pl/api/exchangerates/tables/A/?format=json")
@@ -27,12 +28,14 @@ getDataFromNBP()
         <th>Nazwa waluty</th>
         <th>Kod waluty</th>
         <th>Cena</th>
+        <th>Ilosc waluty do zakupu</th>
         <th></th>
       </tr>
       <tr v-for="(rate, index) in rates">
         <td>{{ rate.currency }}</td>
         <td>{{ rate.code }}</td>
         <td>{{ rate.mid }}</td>
+        <td><input type="number" v-model="iloscWaluty"/></td>
         <td><button @click="kupWalute(index)">Kup</button></td>
       </tr>
     </table>
